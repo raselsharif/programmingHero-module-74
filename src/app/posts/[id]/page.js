259e9a85298c6f7ca1430cc2f171a6fc39864f32
raselsharif <raselsharif9@gmail.com>
2023-12-02 19:03:17 +0600

    added generateStaticParams

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -1,5 +1,16 @@
 import Link from "next/link";
 
+export async function generateStaticParams() {
+  const res = await fetch("http://localhost:5000/post");
+  const posts = await res.json();
+  const ids = posts.slice(0, 4).map((post) => {
+    return {
+      id: post.id,
+    };
+  });
+  console.log(ids);
+  return [ids];
+}
 const PostDetails = async ({ params }) => {
   const res = await fetch(`http://localhost:5000/post/${params.id}`);
   const post = await res.json();
